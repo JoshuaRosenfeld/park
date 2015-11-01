08a3f96ae07c17cd467f71c8866c941082946138
Joshua Rosenfeld <jlr0802@gmail.com>
2015-11-01 18:20:59 -0500

    use generic views

--- park/spots/urls.py
+++ park/spots/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	# ex: /spots/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# ex: /spots/5
-	url(r'^(?P<spot_id>[0-9]+)/$', views.spot, name='spot'),
+	url(r'^(?P<pk>[0-9]+)/$', views.SpotView.as_view(), name='spot'),
 ]
--- park/spots/views.py
+++ park/spots/views.py
@@ -1,13 +1,15 @@
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import User, Spot
 
-def index(request):
-	spot_list = Spot.objects.all()
-	template = loader.get_template('spots/index.html')
-	context = {'spot_list': spot_list}
-	return render(request, 'spots/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'spots/index.html'
+	context_object_name = 'spot_list'
 
-def spot(request, spot_id):
-	spot = get_object_or_404(Spot, pk=spot_id)
-	return render(request, 'spots/detail.html', {'spot': spot})
+	def get_queryset(self):
+		""" Return all spots """
+		return Spot.objects.all()
+
+class SpotView(generic.DetailView):
+	model = Spot
