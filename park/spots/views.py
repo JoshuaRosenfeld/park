a9f36c93f89114a98fd02a5cef1442611e73cda7
Joshua Rosenfeld <jlr0802@gmail.com>
2015-11-02 22:13:03 -0500

    use grid for layout

--- park/spots/views.py
+++ park/spots/views.py
@@ -1,15 +1,20 @@
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import TemplateView
 from .models import User, Spot
 
-class IndexView(generic.ListView):
+class IndexView(TemplateView):
 	template_name = 'spots/index.html'
+
+
+class SpotView(generic.DetailView):
+	model = Spot
+
+class SpotsView(generic.DetailView):
+	template_name = 'spots/spots.html'
 	context_object_name = 'spot_list'
 
 	def get_queryset(self):
 		""" Return all spots """
 		return Spot.objects.all()
-
-class SpotView(generic.DetailView):
-	model = Spot
